178a9813389b7c18b7823d2b1bb14ef96413523f
Ronald <ronaldzamoradev@gmail.com>
2023-08-03 21:07:05 -0500

    stripe setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
-
 import { CheckoutForm } from '@/components'
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_PUBLISHABLE_KEY!)
